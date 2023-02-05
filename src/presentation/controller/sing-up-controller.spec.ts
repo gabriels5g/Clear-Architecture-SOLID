81f4884a8b54c137bf061b377d86a91860aef613
gabriels5g <gabrielspxls@gmail.com>
2023-02-05 17:40:22 -0300

    feat: ensure SingUpController returns 400 if no email is provided

--- src/presentation/controller/sing-up-controller.spec.ts
+++ src/presentation/controller/sing-up-controller.spec.ts
@@ -15,4 +15,19 @@ describe("Sign Controller ", () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new Error("missing param: name"));
   });
+
+  it("Should return 400 if no name is provided", () => {
+    const sut = new SingUpController();
+    const httpsRequest = {
+      body: {
+        name: "any_name",
+        password: "any_pass",
+        passwordConfirmation: "any_password",
+      },
+    };
+    const httpResponse = sut.handle(httpsRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error("missing param: email"));
+  });
 });
