5a82bec02308dec8cbf954bdd9bd51919636870f
gabriels5g <gabrielspxls@gmail.com>
2023-02-06 14:39:31 -0300

    test: ensure SingUpController returns 400 if no password is provided

--- src/presentation/controller/sing-up-controller.spec.ts
+++ src/presentation/controller/sing-up-controller.spec.ts
@@ -30,4 +30,19 @@ describe("Sign Controller ", () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError("email"));
   });
+
+  it("Should return 400 if no password is provided", () => {
+    const sut = new SingUpController();
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email",
+        passwordConfirmation: "any_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("password"));
+  });
 });
