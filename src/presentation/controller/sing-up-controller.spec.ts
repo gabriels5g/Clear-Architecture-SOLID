2b172d073d0bc35763710ce51a7d1ad4be4e3548
gabriels5g <gabrielspxls@gmail.com>
2023-02-06 14:23:38 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/controller/sing-up-controller.spec.ts
+++ src/presentation/controller/sing-up-controller.spec.ts
@@ -4,28 +4,28 @@ import { MissingParamError } from "../errors/missing-param-error";
 describe("Sign Controller ", () => {
   it("Should return 400 if no name is provided", () => {
     const sut = new SingUpController();
-    const httpsRequest = {
+    const httpRequest = {
       body: {
         email: "any_email",
         password: "any_pass",
         passwordConfirmation: "any_password",
       },
     };
-    const httpResponse = sut.handle(httpsRequest);
+    const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError("name"));
   });
 
   it("Should return 400 if no email is provided", () => {
     const sut = new SingUpController();
-    const httpsRequest = {
+    const httpRequest = {
       body: {
         name: "any_name",
         password: "any_pass",
         passwordConfirmation: "any_password",
       },
     };
-    const httpResponse = sut.handle(httpsRequest);
+    const httpResponse = sut.handle(httpRequest);
 
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError("email"));
