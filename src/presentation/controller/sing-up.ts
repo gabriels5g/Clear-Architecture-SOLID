6bdfd7b13b5245759b6f4ff102c412bbb36d7d9d
gabriels5g <gabrielspxls@gmail.com>
2023-02-07 16:53:25 -0300

    test: ensure SingUpController calls EmailValidator with correct email

--- src/presentation/controller/sing-up.ts
+++ src/presentation/controller/sing-up.ts
@@ -7,6 +7,7 @@ import { InvalidParamError } from "../errors/invalid-param-error";
 
 export class SingUpController implements Controller {
   private readonly emailValidator: EmailValidator;
+
   constructor(emailValidator: EmailValidator) {
     this.emailValidator = emailValidator;
   }
@@ -18,7 +19,6 @@ export class SingUpController implements Controller {
       "password",
       "passwordConfirmation",
     ];
-
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field));
